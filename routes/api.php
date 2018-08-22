eaa1213cec656a83e13a44c44b96873704ecae38
huhuapin <867840692@qq.com>
2018-08-22 17:49:40 +0800

    整理api.php

--- routes/api.php
+++ routes/api.php
@@ -19,31 +19,40 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',[
     'namespace' => 'App\Http\Controllers\Api',
 ],function ($api){
+//问卷调查start
     $api->get('question','FeatureController@question');
+//问卷调查end
 
+
+//新生查询start
     $api->get('newstudent','FeatureController@newStudent');
-    //事务中心参观start
-    $api->post('preordain/login','PreordainController@login');
-    $api->post('preordain/adminlogin','PreordainController@adminlogin');
+//新生查询end
+
+
+//事务中心参观start
+    //所有人都可以进行的操作
+    $api->post('preordain/login','PreordainController@login');  //学院登录
+    $api->post('preordain/adminlogin','PreordainController@adminlogin');//管理登录
+    $api->get('preordain/time','PreordainController@lastTime');//获取上次设置的时间
+    $api->get('preordain/list','PreordainController@latestList');//获取所有时间段
+
+    //登陆后才可以进行的操作
+    $api->group(['middleware'=>['auth:preordain']],function ($api) {
+        $api->get('preordain/userinfo','PreordainController@userinfo');//获取登录者的name
+    });
+
     //学院可以进行的操作
     $api->group(['middleware'=>['auth:preordain','preordain.user']],function ($api){
-       //学院预约
-       $api->post('preordain/select','PreordainController@select');
-       //学院删除预约
-       $api->delete('preordain/select/{id}','PreordainController@deleteSelect');
+       $api->post('preordain/select','PreordainController@select');//学院预约
+       $api->delete('preordain/select/{id}','PreordainController@deleteSelect'); //学院删除预约
     });
-    $api->get('preordain/list','PreordainController@latestList');
-    //获取学院信息
-    $api->group(['middleware'=>['auth:preordain']],function ($api) {
-        $api->get('preordain/userinfo','PreordainController@userinfo');
-    });
+
     //管理员可以进行的操作
     $api->group(['middleware'=>['auth:preordain','preordain.admin']],function ($api){
-        //获取上次设置的时间
-        $api->get('preordain/time','PreordainController@lastTime');
-        //设置时间段
-        $api->post('preordain/time','PreordainController@setTime');
-        //更新时间段（只能更新开始预约时间和结束预约时间）
-        $api->put('preordain/time','PreordainController@updateTime');
+        $api->post('preordain/time','PreordainController@setTime');//设置时间段
+        $api->put('preordain/time','PreordainController@updateTime');//更新时间段（只能更新开始预约时间和结束预约时间）
     });
+//事务中心参观end
+
+    
 });
