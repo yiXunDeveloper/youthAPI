1b49fddec7b74147ec78d76b467758c74c673f3d
huhuapin <867840692@qq.com>
2018-10-10 00:41:42 +0800

    含有dinggo api

---
+++ app/Models/OaUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
+use Spatie\Permission\Traits\HasRoles;
+
+class OaUser extends Authenticatable implements JWTSubject
+{
+    use HasRoles;
+    protected $guard_name = 'web';
+    //
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ database/migrations/2018_10_09_211104_create_oa_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOaUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oa_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sdut_id','11');
+            $table->string('password','255');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oa_users');
+    }
+}
