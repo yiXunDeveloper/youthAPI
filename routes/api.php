bb82677887774fe92e977b0e1e93d7f96338e140
huhuapin <867840692@qq.com>
2019-01-21 20:31:16 +0800

    电话簿导入导出

--- routes/api.php
+++ routes/api.php
@@ -117,6 +117,8 @@ $api->version('v1',[
 
     $api->get('oa/phonebooks','OAController@getPhonebooks');  //获取全部电话簿
     $api->post('oa/phonebook','OAController@addPhonebook')->middleware('api.auth'); // 添加电话簿
+    $api->post('oa/phonebook/import','OAController@importPhonebook')->middleware('api.auth'); // 导入电话簿
+    $api->get('oa/phonebook/export','OAController@exportPhonebook')->middleware('api.auth'); //导出电话簿
     $api->delete('oa/phonebook/{phonebook}','OAController@deletePhonebook')->middleware('api.auth');//删除电话簿
 
     $api->get('oa/workloads','OAController@getWorkloads');   //获取当月的任务量
