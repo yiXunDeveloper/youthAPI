9648a9d412d55d068336b1477e57221dba8242ce
aaa <1377869909@qq.com>
2019-11-27 13:53:28 +0800

    '热点文章'

--- app/Http/Controllers/Qq/Article.php
+++ app/Http/Controllers/Qq/Article.php
@@ -76,6 +76,7 @@ class Article extends Controller
         $zans = array_column($array,'article_zan');
         array_multisort($zans,SORT_DESC,$array);
         $arr2 = array_column($zans, 'article_id');
+        dd($arr2);
         $article = QqArticle::whereIn('id',$arr2)
 //            ->select('id')
 //            ->orderBy(DB::raw('FIND_IN_SET(id, "' . implode(",", $arr2) . '"' . ")"))
